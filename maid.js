24befee5b7e265a2e9e313415b549db2a2cff55c
ramkeks-oss <ramkeks@gmail.com>
2025-09-17 15:47:22 +0300

    Update maid.js

--- maid.js
+++ maid.js
@@ -1,5 +1,5 @@
-// maid.js — localhost:8000/maid.html (через FormData)
-const API_URL = "https://script.google.com/macros/s/AKfycbwM-DERytH4Kr0oTAQKU9dXsAJLTCJ7kxqJhXml5OPKZNCfqYA9G7H0mFfK4uqQmpuJqg/exec";
+// maid.js — отправка заявки в Google Sheets (через FormData)
+const API_URL = "https://script.google.com/macros/s/AKfycbwg9u5cjwahtUJeZz6-friLUCSUkJx2aHQzwI_XAQxi0AOVfkoZSTrA17C8ow5JWi0z/exec";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("maidForm");
@@ -15,12 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
                 method: "POST",
                 body: fd
             });
-            alert("✅ Заявка отправлена! Проверь Google Таблицу (лист «Заявки»).");
+            alert("Заявка отправлена! Проверь таблицу Google Sheets (лист «Заявки»).");
             form.reset();
         } catch (err) {
             console.error(err);
-            alert("❌ Ошибка при отправке. Проверь интернет или API.");
+            alert("Ошибка при отправке. Проверь интернет или API.");
         }
     });
 });
-
